7db1eede88fcf1c6fa5dba416372b65e0e5f710d
shyakap48@gmail.com <princemugabe568@gmail.com>
2024-05-25 21:31:21 +0200

    changed headers

--- PolicyClientProject/settings.py
+++ PolicyClientProject/settings.py
@@ -1,17 +1,12 @@
-
 from pathlib import Path
 import dj_database_url
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-import environ
-
 env = environ.Env()
-
 environ.Env.read_env()
-
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-!8@m(9(^=j=s1!g1bdw^z8q@9vlsyrz&^)$dm9#lhiv4%@pku!'
@@ -21,9 +16,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -43,22 +36,18 @@ LOGIN_URL = '/login/'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
 ]
-
-
 
 CORS_ALLOWED_ORIGINS = [
-    "https://plrc.netlify.app/",
-    "http://127.0.0.1:5500/",
+    "https://plrc.netlify.app",
+    "http://127.0.0.1:5500",
 ]
-
 
 ROOT_URLCONF = 'PolicyClientProject.urls'
 
@@ -79,44 +68,20 @@ TEMPLATES = [
 ]
 WSGI_APPLICATION = 'PolicyClientProject.wsgi.application'
 
-
 # Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'rwandaPolicyLinkClientDatabase',
-#         'USER': 'root',
-#         'PASSWORD': '',  # Add your MySQL password here
-#         'HOST': 'localhost',
-#         'PORT': '3306',
-#     }
-# }
-
-
-
-# Database url from remote Railway app 
-
 DATABASES = {
     'default': dj_database_url.parse(env('DATABASE_URL'))
-
-}  
+}
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'princemugabe567@gmail.com'  # Enter your Gmail address
-EMAIL_HOST_PASSWORD = 'entp vlkt zuql cfho'  # Enter your Gmail password
+EMAIL_HOST_USER = 'princemugabe567@gmail.com'
+EMAIL_HOST_PASSWORD = 'entp vlkt zuql cfho'
 EMAIL_DEBUG = True
 
-
-
 # Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -132,31 +97,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
-# https://docs.djangoproject.com/en/5.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
+# Static files
 STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
-# Define the base directory for static files
 STATIC_DIR = BASE_DIR / 'statics'
-
-# Location of static files
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
